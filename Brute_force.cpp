a2f08f878a14ec35d114f0f617b601676eaeffce
NgocIsHere <111295106+NgocIsHere@users.noreply.github.com>
2022-12-16 16:16:14 +0700

    Update Brute_force.cpp

--- Brute_force.cpp
+++ Brute_force.cpp
@@ -1,25 +1,27 @@
 #include "Header.h"
 
-void Brute_force_Cmp(string pat, string txt, long long &cnt_cmp)
+vector<int> Brute_force_Cmp(string pat, string txt, long long &cnt_cmp)
 {
     int M = pat.length();
     int N = txt.length();
- 
+    vector<int> index;
     for (int i = 0; ++cnt_cmp && i <= N - M; i++) {
         int j;
- 
-        for (j = 0; ++cnt_cmp && j < M; j++)
+        for (j = 0; ++cnt_cmp && j < M; j++){
             if (++cnt_cmp && txt[i + j] != pat[j])
                 break;
- 
+        }
+        if (j== M) 
+            index.pushback(i);
         
     }
+    return index;
 }
-void Brute_force(string pat, string txt)
+vector<int> Brute_force(string pat, string txt)
 {
     int M = pat.length();
     int N = txt.length();
- 
+    vector<int> index;
     for (int i = 0; i <= N - M; i++) {
         int j;
  
@@ -28,7 +30,8 @@ void Brute_force(string pat, string txt)
                 break;
  
         if (j== M) 
-            cout <<"Pattern found at index: "<<i;
+            index.push_back(i);
     }
+    return index;
 }
 
