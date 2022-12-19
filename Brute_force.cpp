c141b3ba3513b0fab43fbba8058d40764debe2c1
NgocIsHere <111295106+NgocIsHere@users.noreply.github.com>
2022-12-19 14:51:24 +0700

    Update Brute_force.cpp

--- Brute_force.cpp
+++ Brute_force.cpp
@@ -8,20 +8,26 @@ vector<pair<int, int>> Brute_force_Cmp(string pat, string txt, long long &cnt_cm
     cnt_cmp = 0;
     long long test = 0;
     vector<pair<int, int>> index;
-    for (int i = 0; ++cnt_cmp && i <= N - M; i++) {
+    for (int i = 0; ++cnt_cmp && i <= N - M; i++) 
+    {
         int j;
         for (j = 0; ++cnt_cmp && j < M; j++){
             if (++cnt_cmp && txt[i + j] != pat[j])
                 break;
         }
-        if (++cnt_cmp && j== M) 
-            if(flag==0){
+        if (++cnt_cmp && j== M)
+        {
+            if(flag == 0)
+            {
                 test = cnt_cmp;
                 flag++;
             }
             index.push_back(make_pair(i, i + M - 1));
-            
+        }
     }
-    cnt_cmp = test;
+    if(flag >= 1)
+    {
+        cnt_cmp = test;
+    }
     return index;
 }
