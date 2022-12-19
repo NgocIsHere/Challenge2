7b0e7f4e7cb33b5ac8129cc035ced9e16a2d330d
NgocIsHere <111295106+NgocIsHere@users.noreply.github.com>
2022-12-19 20:41:09 +0700

    Update Boyer_Moore.cpp

--- Boyer_Moore.cpp
+++ Boyer_Moore.cpp
@@ -1,57 +1,58 @@
 #include "Header.h"
 
-void FindLastOccur(string pat,int L[],int size,long long &cnt_cmp)//Vị trí xuất hiện cuối cùng của mỗi kí tự trong chuỗi pat
+void FindLastOccur(string pat, int L[], int size, long long& cnt_cmp)//Vị trí xuất hiện cuối cùng của mỗi kí tự trong chuỗi pat
 {
-	for (int i = 0;cnt_cmp++ && i < 255; i++)// 255 là số kí tự trong bảng mã ascii
+	for (int i = 0; ++cnt_cmp && i < 255; i++)// 255 là số kí tự trong bảng mã ascii
 		L[i] = -1;
-	for (int i = 0;cnt_cmp++ && i < size; i++)
+	for (int i = 0; ++cnt_cmp && i < size; i++)
 	{
 		L[(int)pat[i]] = i;
 	}
 }
 
-vector<pair<int, int>> Boyer_Moore_Search(string pat, string str, long long &cnt_cmp)
+vector<pair<int, int>> Boyer_Moore_Search(string pat, string str, long long& cnt_cmp)
 {
 	vector<pair<int, int>> index;
 	int m = pat.size();
 	int LastOccur[255];
-
-	FindLastOccur(pat, LastOccur, m,cnt_cmp);
-	long long temp;
+	FindLastOccur(pat, LastOccur, m, cnt_cmp);
 	int n = str.size();
 	int s = 0;
 	int time = 0;//Đếm số lần xuất hiện chuỗi con
-
-	while (cnt_cmp++ && s + m <= n)
+	int flag = 0;
+	long long temp;
+	while (++cnt_cmp && s + m <= n)
 	{
 		int j = m - 1;
 
-		while ((cnt_cmp++ && j >= 0) && (cnt_cmp++ && pat[j] == str[s + j]))
+		while ((++cnt_cmp && j >= 0) && (++cnt_cmp && pat[j] == str[s + j]))
 			j--;
 
-		if (cnt_cmp++ && j < 0)
+		if (++cnt_cmp && j < 0)
 		{
-			cout << "Chuoi con xuat hien tai vi tri tu s = " << s << endl;
+			//cout << "Chuoi con xuat hien tai vi tri tu s = " << s << endl;
+			if (flag == 0) {
+				temp = cnt_cmp;
+				flag++;
+			}
 			index.push_back(make_pair(s, s + m - 1));
 			time++;
-			if(time==1){
-				temp = cnt_cmp;
-			}
-			if (cnt_cmp++ && s + m < n)
+			if (++cnt_cmp && s + m < n)
 				s = s + m - LastOccur[(int)str[s + m]];
 			else
 				s = s + 1;
 		}
 		else
 		{
-			s = s + max(1, j - LastOccur[(int)str[s + j]]);
+			s = s + max(1, j - LastOccur[(int)str[s + j]]); ++cnt_cmp;
 		}
 	}
 	if (time == 0)
 	{
 		cout << "Khong ton tai chuoi con trong chuoi ban dau. \n";
 	}
-	else{
+	if(flag>=1)
+	{
 		cnt_cmp = temp;
 	}
 	return index;
